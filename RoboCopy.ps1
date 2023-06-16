97bc2439e02540d96710d721fbb0a2477a1c9249
kendraanthony <109248022+kendraanthony@users.noreply.github.com>
2023-06-16 15:34:25 -0600

    Add files via upload

---
+++ RoboCopy.ps1
@@ -0,0 +1,22 @@
+﻿$log = ("C:\RoboLogs\logs_{0:MMddyyyy_HHmmss}.log" -f (get-date))
+
+
+#create RoboLogs folder in C:
+
+
+
+$optionRobo = @("/R:1","/W:1","/E","/MT:15","/LOG+:$log","/TEE","/ETA","/A-:SH")
+
+
+
+
+
+#Net Use \\xx.xx.xx.xx\ShareName /u:xx.xx.xx.xx\admin
+
+
+
+
+
+Robocopy.exe '\\Source' \\Destination $optionRobo
+
+##remember to change to local non-networking location for destination
